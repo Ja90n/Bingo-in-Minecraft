4fcf60c1923cf21d8f52da1e823eb02b7d723b0a
Ja90n <81578391+Ja90n@users.noreply.github.com>
2024-04-15 22:57:01 +0200

    0.1.3
    updated to 1.20.4 and fixed some spaghetti code

--- src/main/java/com/ja90n/bingo/ConfigManager.java
+++ src/main/java/com/ja90n/bingo/ConfigManager.java
@@ -15,12 +15,33 @@ public class ConfigManager {
     private YamlConfiguration yamlConfiguration;
     private ChatColor chatColor;
     private ItemStack itemStack;
-    private final boolean isAutoHosted;
-    private final int secondsBetweenNumbersCalled, countdownSeconds;
-    private final boolean canJoinDuringGame;
-    private final int minimumPlayers;
+
+    private boolean isAutoHosted;
+    private int secondsBetweenNumbersCalled, countdownSeconds;
+    private boolean canJoinDuringGame;
+    private int minimumPlayers;
+    private final Bingo bingo;
 
     public ConfigManager(Bingo bingo){
+        this.bingo = bingo;
+        initiate();
+    }
+
+    public void initiate() {
+        getFile();
+        setValuesFromConfig();
+        setColour();
+    }
+
+    private void setValuesFromConfig() {
+        countdownSeconds = bingo.getConfig().getInt("countDownLength");
+        secondsBetweenNumbersCalled = bingo.getConfig().getInt("timeBetweenCalls");
+        minimumPlayers = bingo.getConfig().getInt("minimumPlayers");
+        isAutoHosted = bingo.getConfig().getBoolean("autohosting");
+        canJoinDuringGame = bingo.getConfig().getBoolean("canJoinDuringGame");
+    }
+
+    private void getFile() {
         File file = new File(bingo.getDataFolder(), bingo.getConfig().getString("language") + ".yml");
         if (file.exists()){
             yamlConfiguration = YamlConfiguration.loadConfiguration(file);
@@ -28,89 +49,62 @@ public class ConfigManager {
             Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Language file does not exist so the Bingo plugin could not start!");
             bingo.getPluginLoader().disablePlugin(bingo);
         }
+    }
 
-        countdownSeconds = bingo.getConfig().getInt("countDownLength");
-        secondsBetweenNumbersCalled = bingo.getConfig().getInt("timeBetweenCalls");
-        minimumPlayers = bingo.getConfig().getInt("minimumPlayers");
-        isAutoHosted = bingo.getConfig().getBoolean("autohosting");
-        canJoinDuringGame = bingo.getConfig().getBoolean("canJoinDuringGame");
-
+    public void setColour() {
         switch (bingo.getConfig().getString("colour")){
             case "red":
                 chatColor = ChatColor.RED;
-                itemStack = new ItemStack(Material.RED_STAINED_GLASS_PANE);
-                ItemMeta redmeta = itemStack.getItemMeta();
-                redmeta.setDisplayName(" ");
-                itemStack.setItemMeta(redmeta);
+                setItemStack(Material.RED_STAINED_GLASS_PANE);
                 break;
             case "orange":
                 chatColor = ChatColor.GOLD;
-                itemStack = new ItemStack(Material.ORANGE_STAINED_GLASS_PANE);
-                ItemMeta orangemeta = itemStack.getItemMeta();
-                orangemeta.setDisplayName(" ");
-                itemStack.setItemMeta(orangemeta);
+                setItemStack(Material.ORANGE_STAINED_GLASS_PANE);
                 break;
             case "lime":
                 chatColor = ChatColor.GREEN;
-                itemStack = new ItemStack(Material.LIME_STAINED_GLASS_PANE);
-                ItemMeta limemeta = itemStack.getItemMeta();
-                limemeta.setDisplayName(" ");
-                itemStack.setItemMeta(limemeta);
+                setItemStack(Material.LIME_STAINED_GLASS_PANE);
                 break;
             case "green":
                 chatColor = ChatColor.DARK_GREEN;
-                itemStack = new ItemStack(Material.GREEN_STAINED_GLASS_PANE);
-                ItemMeta greenmeta = itemStack.getItemMeta();
-                greenmeta.setDisplayName(" ");
-                itemStack.setItemMeta(greenmeta);
+                setItemStack(Material.GREEN_STAINED_GLASS_PANE);
                 break;
             case "light_blue":
                 chatColor = ChatColor.BLUE;
-                itemStack = new ItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
-                ItemMeta light_bluemeta = itemStack.getItemMeta();
-                light_bluemeta.setDisplayName(" ");
-                itemStack.setItemMeta(light_bluemeta);
+                setItemStack(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
                 break;
             case "blue":
                 chatColor = ChatColor.BLUE;
-                itemStack = new ItemStack(Material.BLUE_STAINED_GLASS_PANE);
-                ItemMeta bluemeta = itemStack.getItemMeta();
-                bluemeta.setDisplayName(" ");
-                itemStack.setItemMeta(bluemeta);
+                setItemStack(Material.BLUE_STAINED_GLASS_PANE);
                 break;
             case "pink":
                 chatColor = ChatColor.LIGHT_PURPLE;
-                itemStack = new ItemStack(Material.PINK_STAINED_GLASS_PANE);
-                ItemMeta pinkmeta = itemStack.getItemMeta();
-                pinkmeta.setDisplayName(" ");
-                itemStack.setItemMeta(pinkmeta);
+                setItemStack(Material.PINK_STAINED_GLASS_PANE);
                 break;
             case "purple":
                 chatColor = ChatColor.DARK_PURPLE;
-                itemStack = new ItemStack(Material.PURPLE_STAINED_GLASS_PANE);
-                ItemMeta purplemeta = itemStack.getItemMeta();
-                purplemeta.setDisplayName(" ");
-                itemStack.setItemMeta(purplemeta);
+                setItemStack(Material.PURPLE_STAINED_GLASS_PANE);
                 break;
             case "black":
                 chatColor = ChatColor.DARK_GRAY;
-                itemStack = new ItemStack(Material.BLACK_STAINED_GLASS_PANE);
-                ItemMeta blackmeta = itemStack.getItemMeta();
-                blackmeta.setDisplayName(" ");
-                itemStack.setItemMeta(blackmeta);
+                setItemStack(Material.BLACK_STAINED_GLASS_PANE);
                 break;
             case "grey":
             case "gray":
                 chatColor = ChatColor.GRAY;
-                itemStack = new ItemStack(Material.GRAY_STAINED_GLASS_PANE);
-                ItemMeta greymeta = itemStack.getItemMeta();
-                greymeta.setDisplayName(" ");
-                itemStack.setItemMeta(greymeta);
+                setItemStack(Material.GRAY_STAINED_GLASS_PANE);
                 break;
             default:
                 Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Color does not exist so the Bingo plugin could not start!");
                 bingo.getPluginLoader().disablePlugin(bingo);
         }
+    }
+
+    private void setItemStack(Material material) {
+        itemStack = new ItemStack(material);
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.setDisplayName(" ");
+        itemStack.setItemMeta(itemMeta);
     }
 
     public int getSecondsBetweenNumbersCalled(){
